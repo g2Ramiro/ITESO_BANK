d83ed2f6999263e9ea398a189063c335a2edda68
g2Ramiro <145502668+g2Ramiro@users.noreply.github.com>
2025-10-30 08:46:54 -0600

    Conexion a MongoDB

--- connect.py
+++ connect.py
@@ -1,6 +1,10 @@
 import os
 from cassandra.cluster import Cluster
+from pymongo import MongoClient
 
 CLUSTER_IPS = os.getenv('CASSANDRA_CLUSTER_IPS', '127.0.0.1')
 KEYSPACE = os.getenv('CASSANDRA_KEYSPACE', 'logistics')
 REPLICATION_FACTOR = os.getenv('CASSANDRA_REPLICATION_FACTOR', '1')
+client = MongoClient('mongodb://localhost:27017/')
+db = client.ItesoBank
+
