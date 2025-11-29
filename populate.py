6c52921eb93637eb297b78301a8a8f587a5e0330
g2Ramiro <145502668+g2Ramiro@users.noreply.github.com>
2025-11-29 12:42:21 -0600

    Enhance populate.py to include Dgraph population
    Se añadió la función populate_dgraph que llama a las funciones de mode.py cuando se llama a populate en main.

--- populate.py
+++ populate.py
@@ -1,22 +1,32 @@
 # populate.py
-
 from cassandra.cluster import Cluster
-from connect import CLUSTER_IPS, KEYSPACE, REPLICATION_FACTOR
+import connect
 from Cassandra.loader import create_keyspace_and_tables, load_all_data
+from Dgraph import model as mo
 
 def populate_cassandra():
-    ips = [ip.strip() for ip in CLUSTER_IPS.split(",")]
+    ips = [ip.strip() for ip in connect.CLUSTER_IPS.split(",")]
     cluster = Cluster(ips)
     session = cluster.connect()
 
-    rf = int(REPLICATION_FACTOR)
-    create_keyspace_and_tables(session, KEYSPACE, rf)
-
-    # usa la carpeta data/Cassandra donde estan los CSV
-    load_all_data(session, KEYSPACE, base_path="data/Cassandra")
+    rf = int(connect.REPLICATION_FACTOR)
+    create_keyspace_and_tables(session, connect.KEYSPACE, rf)
+    load_all_data(session, connect.KEYSPACE, base_path="data/Cassandra")
 
     print("✅ Cassandra poblada correctamente.")
     cluster.shutdown()
 
+def populate_dgraph():
+    print("🚀 Conectando a Dgraph...")
+    client_stub = connect.create_client_stub()
+    client = connect.create_client(client_stub)
+
+    try:
+        mo.create_schema(client)
+        mo.load_data(client)
+    finally:
+        client_stub.close()
+
 if __name__ == "__main__":
     populate_cassandra()
+    populate_dgraph()
